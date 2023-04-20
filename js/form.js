38267bb20218e2aed279e2462fd306a4c34c96eb
Delusionmik <119353885+Delusionmik@users.noreply.github.com>
2023-04-20 16:38:41 -0300

    Modificado

--- js/form.js
+++ js/form.js
@@ -6,12 +6,12 @@ var nombre = document.getElementById("name");
 var email = document.getElementById("email");
 var telefono = document.getElementById("telefono");
 var textArea = document.getElementById("contacttextarea");
-var form = document.getElementById ("form");
-var parrafo =document.getElementById("warnings");
+var form = document.getElementById("form");
+var parrafo = document.getElementById("warnings");
 
-form.addEventListener("submit", e=>{
+form.addEventListener("submit", e => {
 	e.preventDefault()
-	let warnings="";
+	let warnings = "";
 	let entrar = false;
 	let regexName = /^[a-zA-ZÀ-ÿ\s]{1,20}$/;
 	let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
@@ -21,23 +21,40 @@ form.addEventListener("submit", e=>{
 	if (!regexName.test(nombre.value)) {
 		warnings += `El nombre no es valido <br>`;
 		entrar = true;
-	} 
-	if (!regexEmail.test(email.value)){
+	}
+	if (!regexEmail.test(email.value)) {
 		warnings += `El email no es valido <br>`;
-		entrar =true;
+		entrar = true;
 	}
-	if (!regexTel.test(telefono.value)){
+	if (!regexTel.test(telefono.value)) {
 		warnings += `El telefono no es valido <br>`;
-		entrar =true;
+		entrar = true;
 	}
-	if (!regexParra.test(textArea.value)){
+	if (!regexParra.test(textArea.value)) {
 		warnings += `El texto es muy corto <br>`;
-		entrar =true;
+		entrar = true;
 	}
 	if (entrar) {
 		parrafo.innerHTML = warnings;
 	} else {
-		swal("Formulario enviado", "Muchas gracias por suscribirte!");
-		document.getElementById("form").reset();
+		var $form = document.querySelector("#form");
+
+		$form.addEventListener("submit", handleSubmit)
+
+		async function handleSubmit(event) {
+			event.preventDefault()
+			const form = new FormData(this)
+			const response = await fetch (this.action, {
+				method: this.method,
+				body: form,
+				headers: {
+					"Accept": "application/json"
+				}
+			})
+			if (response.ok) {
+				document.getElementById("form").reset()
+				swal("Formulario enviado", "Muchas gracias por suscribirte!");
+			}
+		}
 	}
 })
